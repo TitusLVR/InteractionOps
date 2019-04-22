9ca1d55715dcd6d66c0cc5fd34db0d831de133a2
Furash <cg.furash@gmail.com>
2019-04-22 20:07:00 +0200

    modal aligner  ui updated

--- __init__.py
+++ __init__.py
@@ -13,44 +13,46 @@ bl_info = {
 import bpy
 
 from .operators.iops import IOPS
-from .operators.modes import (IOPS_OT_MODE_F1, 
+from .operators.modes import (IOPS_OT_MODE_F1,
                               IOPS_OT_MODE_F2,
                               IOPS_OT_MODE_F3,
                               IOPS_OT_MODE_F4)
 
 from .prefs.addon_preferences import IOPS_AddonPreferences
-
 from .utils.cursor_origin import *
 from .utils.align_object_to_face import *
 
+
 # WarningMessage
-def ShowMessageBox(text = "", title = "WARNING", icon = "ERROR"):
+def ShowMessageBox(text="", title="WARNING", icon="ERROR"):
     def draw(self, context):
-        self.layout.label(text = text)        
-    bpy.context.window_manager.popup_menu(draw, title = title, icon = icon)
+        self.layout.label(text=text)
+    bpy.context.window_manager.popup_menu(draw, title=title, icon=icon)
+
 
 def register_keymaps():
     keymapItems = (bpy.context
-                  .window_manager
-                  .keyconfigs
-                  .addon
-                  .keymaps
-                  .new("Window")
-                  .keymap_items)
+                      .window_manager
+                      .keyconfigs
+                      .addon
+                      .keymaps
+                      .new("Window")
+                      .keymap_items)
 
     kmi = keymapItems.new('iops.mode_f1', 'F1', 'PRESS')
-    kmi.active = True     
-    kmi = keymapItems.new('iops.mode_f2', 'F2', 'PRESS')    
     kmi.active = True
-    kmi = keymapItems.new('iops.mode_f3', 'F3', 'PRESS')    
+    kmi = keymapItems.new('iops.mode_f2', 'F2', 'PRESS')
     kmi.active = True
-    kmi = keymapItems.new('iops.mode_f4', 'F4', 'PRESS')    
+    kmi = keymapItems.new('iops.mode_f3', 'F3', 'PRESS')
     kmi.active = True
-    kmi = keymapItems.new('iops.cursor_origin', 'F4', 'PRESS')    
+    kmi = keymapItems.new('iops.mode_f4', 'F4', 'PRESS')
     kmi.active = True
-    kmi = keymapItems.new('iops.align_object_to_face', 'F6', 'PRESS')    
+    kmi = keymapItems.new('iops.cursor_origin', 'F4', 'PRESS')
     kmi.active = True
-     
+    kmi = keymapItems.new('iops.align_object_to_face', 'F6', 'PRESS')
+    kmi.active = True
+
+
 def unregister_keymaps():
     allKeymaps = bpy.context.window_manager.keyconfigs.addon.keymaps
     keymap = allKeymaps.get("Window")
@@ -60,14 +62,14 @@ def unregister_keymaps():
                 item for item in keymapItems if item.idname.startswith('iops.')
             )
         for item in toDelete:
-            keymapItems.remove(item)     
+            keymapItems.remove(item)
 
 # Classes for reg and unreg
 classes = (IOPS,
            IOPS_AddonPreferences,
            IOPS_OT_MODE_F1,
            IOPS_OT_MODE_F2,
-           IOPS_OT_MODE_F3,             
+           IOPS_OT_MODE_F3,
            IOPS_OT_MODE_F4,
            CursorOrigin,
            AlignObjectToFace,
@@ -75,15 +77,17 @@ classes = (IOPS,
 
 reg_cls, unreg_cls = bpy.utils.register_classes_factory(classes)
 
+
 def register():
     reg_cls()
     register_keymaps()
     print("IOPS Registered?!")
 
+
 def unregister():
-   unreg_cls()
-   unregister_keymaps()
-   print("IOPS Unregistered!")
-    
+    unreg_cls()
+    unregister_keymaps()
+    print("IOPS Unregistered!")
+
 if __name__ == "__main__":
     register()
--- utils/align_object_to_face.py
+++ utils/align_object_to_face.py
@@ -1,45 +1,74 @@
 import bpy
 import blf
-from bpy.props import (IntProperty, 
-                       FloatProperty, 
-                       BoolProperty, 
-                       StringProperty, 
+from bpy.props import (IntProperty,
+                       FloatProperty,
+                       BoolProperty,
+                       StringProperty,
                        FloatVectorProperty)
 import bmesh
 import math
+from math import radians, degrees
 from mathutils import Vector, Matrix, Euler
 
+
 def draw_callback_px(self, context):
-        _location = "Location: x = {0:.4f}, y = {1:.4f}, z = {2:.4f}"
-        _axis_move = "Move Axis: " + str(self.axis_move)
+        _location   = "Location: x = {0:.4f}, y = {1:.4f}, z = {2:.4f}"
+        _rotation   = "Rotation: x = {0:.4f}, y = {1:.4f}, z = {2:.4f}"
+        _align_edge = "Edge index: {0}"
+        _axis_move  = "Move Axis: " + str(self.axis_move)
 
         # Font
-        font_id = 0
-        blf.size(font_id, 20, 72)
+        font = 0
+        blf.size(font, 20, 72)
+
+        # Move axis text overlay
+        blf.position(font, 60, 120, 0)
+        blf.draw(font, _align_edge.format(self.get_edge_idx(self.counter)))
 
         # Active axis text overlay
-        blf.position(font_id, 15, 90, 0)
-        blf.draw(font_id, _axis_move)
+        blf.position(font, 60, 90, 0)
+        blf.draw(font, _axis_move)
 
         # Location text overlay
-        blf.position(font_id, 15, 60, 0)
-        blf.draw(font_id, _location.format(self.loc[0], self.loc[1], self.loc[2]))
+        blf.position(font, 60, 60, 0)
+        blf.draw(font, _location.format(self.loc[0], self.loc[1], self.loc[2]))
+        
+        # Location text overlay
+        blf.position(font, 60, 30, 0)
+        blf.draw(font, _rotation.format(degrees(self.rot[0]),
+                                        degrees(self.rot[1]),
+                                        degrees(self.rot[2])
+                                        ))
 
 
 class AlignObjectToFace(bpy.types.Operator):
     """ Align object to selected face """
     bl_idname = "iops.align_object_to_face"
     bl_label = "iOps Align object to face"
-    bl_options = {"REGISTER","UNDO"}
+    bl_options = {"REGISTER", "UNDO"}
 
-    axis_align  : StringProperty()
-    axis_move   : StringProperty()
-    loc         : FloatVectorProperty()
-    loc_start   : FloatVectorProperty()
+    axis_move       : StringProperty()
+    axis_rotate     : StringProperty()
+    loc             : FloatVectorProperty()
+    loc_start       : FloatVectorProperty()
+    edge_idx        : IntProperty()
+    counter         : IntProperty()
+    flip            : BoolProperty()
+
+    rot = Euler()
+    rot_start = Euler()
 
     @classmethod
-    def poll(self,context):
-        return len(context.selected_objects) > 0 
+    def poll(self, context):
+        return len(context.selected_objects) > 0
+
+    def rotate(self, axis):
+        if axis == 'X':
+            self.rot[0] += radians(90)
+        elif axis == 'Y':
+            self.rot[1] += radians(90)
+        elif axis == 'Z':
+            self.rot[2] += radians(90)
 
     def move(self, axis_move, step):
         if axis_move == 'X':
@@ -48,37 +77,48 @@ class AlignObjectToFace(bpy.types.Operator):
             self.loc[1] += step
         elif axis_move == 'Z':
             self.loc[2] += step
-        else: pass  
-           
-    def align_to_face(self, context):   
+
+    def get_edge_idx(self, idx):
+        """Return edge index from (counter % number of edges) of a face"""
+        obj = bpy.context.active_object
+        polymesh = obj.data
+        bm = bmesh.from_edit_mesh(polymesh)
+        face = bm.faces.active
+        face.edges.index_update()
+        index = abs(idx % len(face.edges))
+
+        return index
+
+    def align_to_face(self, idx, flip):
         """ Takes face normal and aligns it to global axis.
-            Uses one of the face edges to further align it to another axis.""" 
+            Uses one of the face edges to further align it to another axis."""
 
-        obj = context.active_object
+        obj = bpy.context.active_object
         mx = obj.matrix_world
-        loc = mx.to_translation()           #  Store location   
-        polymesh = obj.data            
-        bm = bmesh.from_edit_mesh(polymesh)    
-
-        # Get active face
+        loc = mx.to_translation()  # Store location
+        polymesh = obj.data
+        bm = bmesh.from_edit_mesh(polymesh)
         face = bm.faces.active
 
-        # Build vectors for new matrix               
-        n = face.normal                # Z
-        t = face.calc_tangent_edge()   # Y
-        c = t.cross(n)                 # X
+        # Vector from and edge
+        vector_edge = (face.edges[idx].verts[0].co - face.edges[idx].verts[1].co).normalized()
 
-        # Assemble new matrix    
-        mx_rot = Matrix((c, t, n)).transposed().to_4x4() 
+        # Build vectors for new matrix
+        n = face.normal if flip else (face.normal * -1)        # Z
+        t = vector_edge                                        # Y
+        c = t.cross(n)                                         # X
+
+        # Assemble new matrix
+        mx_rot = Matrix((c, t, n)).transposed().to_4x4()
         obj.matrix_world = mx_rot.inverted()
-        obj.location = loc 
+        obj.location = loc
 
-    def modal(self, context, event):  
+    def modal(self, context, event):
         context.area.tag_redraw()
-        # Moving object while SHIFT is pressed for testing purpose     
+        # Moving object while SHIFT is pressed for testing purpose
         # ---------------------------------------------------------
         if event.shift:
-            if event.type in {'X','Y','Z'} and event.value == "PRESS":
+            if event.type in {'X', 'Y', 'Z'} and event.value == "PRESS":
                 self.axis_move = event.type
                 bpy.context.object.location = self.loc
                 print("Changed to: " + self.axis_move)
@@ -92,17 +132,29 @@ class AlignObjectToFace(bpy.types.Operator):
                 self.move(self.axis_move, 0.5)
                 bpy.context.object.location = self.loc
                 print("Moving along: " + self.axis_move)
-        # ---------------------------------------------------------
 
-        if event.type in {'X','Y','Z'} and event.value == "PRESS":
-                self.axis_move = event.type
-                print(event.type,"pressed")
+        # ---------------------------------------------------------
+        elif event.type in {'X', 'Y', 'Z'} and event.value == "PRESS":
+                self.axis_rotate = event.type
+                self.rotate(event.type)
+                self.report({"INFO"}, event.type)
 
         elif event.type == "WHEELDOWNMOUSE":
-                print(event.type)
+                if self.counter > 1:
+                    self.counter -= 1
+
+                self.align_to_face(self.get_edge_idx(self.counter), self.flip)
+                self.report({"INFO"}, event.type)
 
         elif event.type == "WHEELUPMOUSE":
-                print(event.type)
+                self.counter += 1
+                self.align_to_face(self.get_edge_idx(self.counter), self.flip)
+                self.report({"INFO"}, event.type)
+
+        elif event.type == 'F' and event.value == "PRESS":
+                self.flip = not self.flip
+                self.align_to_face(self.get_edge_idx(self.counter), self.flip)
+                self.report({"INFO"}, event.type)
 
         elif event.type in {"LEFTMOUSE", "SPACE"}:
             bpy.types.SpaceView3D.draw_handler_remove(self._handle, "WINDOW")
@@ -111,29 +163,37 @@ class AlignObjectToFace(bpy.types.Operator):
         elif event.type in {"RIGHTMOUSE", "ESC"}:
             bpy.types.SpaceView3D.draw_handler_remove(self._handle, "WINDOW")
             bpy.context.object.location = self.loc_start
+            bpy.context.object.rotation_euler = self.rot_start
             return {"CANCELLED"}
 
         return {"RUNNING_MODAL"}
 
     def invoke(self, context, event):
-        if context.object and context.area.type == "VIEW_3D":    
-            # Initialize axis and assign starting values for object's location     
-            self.axis_move = 'Z'                                  
-            self.loc_start = bpy.context.object.location  
+        if context.object and context.area.type == "VIEW_3D":
+            # Initialize axis and assign starting values for object's location
+            self.axis_move = 'Z'
+            self.axis_rotate = 'Z'
+            self.flip = False
+            self.loc_start = bpy.context.object.location
+            self.rot_start = bpy.context.object.rotation_euler
             self.loc = self.loc_start
+            self.rot = self.rot_start
+            self.edge_idx = 1
+            self.counter = 0
+
+            self.align_to_face(self.edge_idx, self.flip)
 
             # Add drawing handler for text overlay rendering
             args = (self, context)
             self._handle = bpy.types.SpaceView3D.draw_handler_add(
-                            draw_callback_px, 
-                            args, 
-                            'WINDOW', 
+                            draw_callback_px,
+                            args,
+                            'WINDOW',
                             'POST_PIXEL')
-            
+
             # Add modal handler to enter modal mode
             context.window_manager.modal_handler_add(self)
             return {"RUNNING_MODAL"}
         else:
             self.report({"WARNING"}, "No active object, could not finish")
             return {"CANCELLED"}
-
