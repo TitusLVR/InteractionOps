bca1a858fb7fa8c4a58e474c040a673b7f930682
Furash <cg.furash@gmail.com>
2019-05-04 08:16:02 +0200

    points render wip

--- operators/curve_subdivide.py
+++ operators/curve_subdivide.py
@@ -14,39 +14,29 @@ from mathutils import Vector, Matrix, Euler
 
 
 def draw_ui(self, context):       
-        _points = "Number of cuts: {0}" 
-        # Font
-        font = 0
-        blf.size(font, 20, 72)   
-        # Curve subdivide points
-        blf.position(font, 60, 30, 0),
-        blf.draw(font, _points.format(self.points_num))
+    _points = "Number of cuts: {0}" 
+    # Font
+    font = 0
+    blf.size(font, 20, 72)   
+    # Curve subdivide points
+    blf.position(font, 60, 30, 0),
+    blf.draw(font, _points.format(self.points_num))
 
 # TODO SORT THIS OUT ##############################
-def draw_preview_pts(self, context):
-    coords = preview_curve_pts()
-    pass
 
-def preview_curve_pts(points):
-        obj = bpy.context.active_object
-        pt_a = points[0]
-        pt_b = points[1]
-        draw_pts = []
 
-        # Store selected curve points
-        if obj.type == "CURVE":
-            selected_pts = []
-            for s in obj.data.splines:
-                for b in s.bezier_points:
-                    if b.select_control_point:
-                        selected_pts.append(b)
+def draw_curve_pts(self, context):
+    coords = self.get_curve_pts()
+    shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
+    batch = batch_for_shader(shader, 'LINES', {"pos": coords})
+    shader.bind()
+    shader.uniform_float("color", (1, 1, 0, 1))
+    batch.draw(shader)
 
-        draw_pts = selected_pts  # Test
-        return draw_pts
 ##################################################
 
 class IOPS_OT_CurveSubdivide(bpy.types.Operator):
-    """ Align object to selected face """
+    """ Subdivide Curve """
     bl_idname = "iops.curve_subdivide"
     bl_label = "iOps curve subdivide"
     bl_options = {"REGISTER", "UNDO"}
@@ -59,7 +49,8 @@ class IOPS_OT_CurveSubdivide(bpy.types.Operator):
 
     @classmethod
     def poll(self, context):
-         return (context.active_object.type == 'CURVE')
+         return (context.active_object.type == "CURVE" and
+                context.active_object.mode == "EDIT")
 
     def execute(self, context):
         self.subdivide(self.points_num)
@@ -69,6 +60,17 @@ class IOPS_OT_CurveSubdivide(bpy.types.Operator):
         obj = bpy.context.active_object
         self.points_num = points
         bpy.ops.curve.subdivide(number_cuts=self.points_num)
+
+    def get_curve_pts(self):
+        obj = bpy.context.active_object
+        selected_pts = []
+        # Store selected curve points
+        if obj.type == "CURVE":
+            for s in obj.data.splines:
+                for b in s.bezier_points:
+                    if b.select_control_point:
+                        selected_pts.append(b.co)       
+        return selected_pts
 
     def modal(self, context, event):
         context.area.tag_redraw()
@@ -88,11 +90,13 @@ class IOPS_OT_CurveSubdivide(bpy.types.Operator):
 
         elif event.type in {"LEFTMOUSE", "SPACE"} and event.value == "PRESS":
             self.execute(context)            
-            bpy.types.SpaceView3D.draw_handler_remove(self._handle, "WINDOW")            
+            bpy.types.SpaceView3D.draw_handler_remove(self._handle_ui, "WINDOW")            
+            bpy.types.SpaceView3D.draw_handler_remove(self._handle_curve, "WINDOW")            
             return {"FINISHED"}
 
         elif event.type in {"RIGHTMOUSE", "ESC"}:
-            bpy.types.SpaceView3D.draw_handler_remove(self._handle, "WINDOW")           
+            bpy.types.SpaceView3D.draw_handler_remove(self._handle_ui, "WINDOW")           
+            bpy.types.SpaceView3D.draw_handler_remove(self._handle_curve, "WINDOW")           
             return {"CANCELLED"}
 
         return {"RUNNING_MODAL"}
@@ -103,11 +107,17 @@ class IOPS_OT_CurveSubdivide(bpy.types.Operator):
 
             # Add drawing handler for text overlay rendering
             args = (self, context)
-            self._handle = bpy.types.SpaceView3D.draw_handler_add(
+            self._handle_ui = bpy.types.SpaceView3D.draw_handler_add(
                             draw_ui,
                             args,
                             'WINDOW',
                             'POST_PIXEL')
+        
+            self._handle_curve = bpy.types.SpaceView3D.draw_handler_add(
+                            draw_curve_pts,
+                            args,
+                            'WINDOW',
+                            'POST_VIEW')
 
             # Add modal handler to enter modal mode
             context.window_manager.modal_handler_add(self)
@@ -115,12 +125,3 @@ class IOPS_OT_CurveSubdivide(bpy.types.Operator):
         else:
             self.report({"WARNING"}, "No active object, could not finish")
             return {"CANCELLED"}
-        
-def register():
-    bpy.utils.register_class(CurveSubdivide)
-
-def unregister():
-    bpy.utils.unregister_class(CurveSubdivide)
-
-if __name__ == "__main__":
-    register()
