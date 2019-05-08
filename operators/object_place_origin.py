45bbe4a34ffcb0bd3fe2ddcc12cafce6d17b7444
TitusLVR <titus.mailbox@gmail.com>
2019-05-08 15:27:14 +0200

    Place origin BBox

--- operators/object_place_origin.py
+++ operators/object_place_origin.py
@@ -8,6 +8,7 @@ import math
 from math import radians, degrees
 from mathutils import Vector, Matrix, Euler
 from math import sin, cos, pi
+import numpy
 
 
 # get circle vertices on pos 2D by segments
@@ -32,6 +33,25 @@ def GenerateCircleTris(segments, startID):
     tricomp = (startID, tri, startID+1)
     triangles.append(tricomp)
     return triangles
+
+# draw a circle on scene
+def draw_circle_fill_2d(self,context):
+    point = self.target
+    if point != (0,0):
+        position = point    
+        color = (1, 0, 0, 1)
+        radius = 6
+        segments= 12
+        # create vertices
+        coords = GenerateCircleVerts(position, radius, segments)
+        # create triangles
+        triangles = GenerateCircleTris(segments, 0)
+        # set shader and draw
+        shader = gpu.shader.from_builtin('2D_UNIFORM_COLOR')
+        batch = batch_for_shader(shader, 'TRIS', {"pos": coords}, indices=triangles)
+        shader.bind()
+        shader.uniform_float("color", color)
+        batch.draw(shader)
 
 ## draw multiple circle in one batch on screen
 def draw_multicircles_fill_2d_Face(self, context):    
@@ -61,8 +81,8 @@ def draw_multicircles_fill_2d_Face(self, context):
 def draw_multicircles_fill_2d_BBOX(self, context):    
     positions = (self.ObjectBBox(context))[2]
     if positions != None: 
-        color = (0.973, 0.723, 0.15, 0.1)
-        radius = 5
+        color = (0.873, 0.623, 0.15, 0.1)         
+        radius = 3
         segments= 16
         coords = []
         triangles = []  
@@ -77,7 +97,7 @@ def draw_multicircles_fill_2d_BBOX(self, context):
         # set shader and draw
         shader = gpu.shader.from_builtin('2D_UNIFORM_COLOR')
         batch = batch_for_shader(shader, 'TRIS', {"pos": coords}, indices=triangles)
-        shader.bind()
+        shader.bind()        
         shader.uniform_float("color", color)
         batch.draw(shader)
 
@@ -93,29 +113,56 @@ def draw_bbox_lines(self,context):
         elif len(coords) > 8:  
             indices = (
                         #Bbox
-                        (0, 1), (1, 2), (2, 3), (3, 0),
-                        (4, 5), (5, 6), (6, 7), (7, 4),
-                        (0, 4), (1, 5), (2, 6), (3, 7),
+                        #X-
+                        (0, 1), (1, 2), (2, 3), (0,3),
+                        #X+
+                        (4, 5), (5, 6), (6, 7), (4,7),
+                        #Y- BTM
+                        (0, 4),
+                        #Y- UP
+                        (1, 5),
+                        #Y-BTM
+                        (3, 7),
+                        #Y+UP
+                        (2, 6),
+                        
                         #SUBD
-                        #(8, 10), (9, 11), (12, 14), (13,15),
-                        #(16,17), (17, 18),(19,16),(11,15),
-                        #(12,8)                        
+                        #X-
+                        (8, 10), (9, 11),
+                        #X+
+                        (12, 14), (13, 15),
+                        #Y-
+                        (16, 17), (8, 12), 
+                        #Y+
+                        (10, 14), (18, 19),                        
+                        #Z-
+                        (11, 15), (16, 19),
+                        #Z+
+                        (17, 18),(9,13),
+                        
+                        #Center
+                        #+X
+                        (20,23),
+                        #-X
+                        (20,21),
+                        #+Y
+                        (20,27),
+                        #-Y
+                        (20,25),
+                        #+Z
+                        (20,31),                        
+                        #-Z
+                        (20,29),
                         )
         else:
             indices = ()
         shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
         batch = batch_for_shader(shader, 'LINES', {"pos": coords}, indices=indices)    
         shader.bind()
-        shader.uniform_float("color", (0.873, 0.623, 0.15, 1))
+        shader.uniform_float("color", (0.573, 0.323, 0.15, 1))
         batch.draw(shader)
 
-#Calculate distance between raycasts
-def calc_distance(step,old_loc,new_loc):
-        distance = math.sqrt(((old_loc[0]-new_loc[0])**2)+((old_loc[1]-new_loc[1])**2))
-        if step <= distance:
-            return True
-        else:
-            return False
+
 
 #def draw_face_pts(self,context):
 #    print ("CALLBACK POS",self.getFaceVertPos(context))    
@@ -135,13 +182,38 @@ class IOPS_OP_PlaceOrigin(bpy.types.Operator):
     bl_label = "Place origin"   
 
 
-    mouse_pos = [0,0]
+    mouse_pos = (0,0)
     result = False
-    result_obj = None   
-       
-        
-    def getClosestPoint(self,context):
-        coord = self.mouse_pos        
+    result_obj = None
+    posBatch = []
+    posBatch3D = []
+    BatchIDX = None 
+    target = (0,0)
+     
+    
+    def PlaceOrigin(self, context):
+        pos = self.posBatch3D[self.BatchIDX]
+        context.scene.cursor.location = pos       
+        bpy.ops.object.origin_set(type="ORIGIN_CURSOR") 
+           
+    #Calculate distance between raycasts
+    def calc_distance(self,context):        
+        mouse_pos = self.mouse_pos
+        posBatch = self.posBatch        
+        if len(posBatch) != 0:            
+            actDist = numpy.linalg.norm(posBatch[0]-Vector(mouse_pos))
+            actID = 0
+            counter = 1
+            itertargets = iter(self.posBatch)
+            next(itertargets)
+            for pos in itertargets:
+                dist = numpy.linalg.norm(pos-Vector(mouse_pos))
+                if dist < actDist:
+                    actID = counter
+                    actDist = dist
+                counter += 1
+            self.BatchIDX = actID 
+            self.target = posBatch[actID]
         
     def SceneRayCast(self,context):
         # get the context arguments        
@@ -191,10 +263,14 @@ class IOPS_OP_PlaceOrigin(bpy.types.Operator):
             
             bbox = object.bound_box
             bbox_Verts3D =[]
-            if len(bbox) != 0:         
-                bbox_Edges = ((0, 1), (1, 2), (2, 3), (3, 0),
-                              (4, 5), (5, 6), (6, 7), (7, 4),
-                              (0, 4), (1, 5), (2, 6), (3, 7))
+            if len(bbox) != 0:
+                SubD_Vert_POS = []         
+                bbox_Edges = ((0, 1), (1, 2), (2, 3), (0, 3),
+                              (4, 5), (5, 6), (6, 7), (4, 7),
+                              (0, 4), (1, 5), (2, 6), (3, 7),(0,6))
+                bbox_SubD_Edges =((8, 10), (9, 11),(12, 14), (13, 15),
+                                  (16, 17), (8, 12), (10, 14), (18, 19),
+                                  (11, 15), (16, 19),(17, 18), (9,13))
                 #BBox
                 for v in bbox:                
                     pos = Vector(v) @  matrix_trans               
@@ -203,15 +279,23 @@ class IOPS_OP_PlaceOrigin(bpy.types.Operator):
                 for e in bbox_Edges:                    
                     vert1 = Vector(bbox[(e[0])])
                     vert2 = Vector(bbox[(e[1])])
-                    vertmid = (vert1+vert2)/2
+                    vertmid = (vert1+vert2)/2                    
                     pos = Vector(vertmid) @  matrix_trans
-                    bbox_Verts3D.append(pos)                                 
+                    bbox_Verts3D.append(pos)
+                for e in bbox_SubD_Edges:
+                    vert1 = Vector(bbox_Verts3D[(e[0])])
+                    vert2 = Vector(bbox_Verts3D[(e[1])])
+                    vertmid = (vert1+vert2)/2                    
+                    pos = Vector(vertmid) 
+                    bbox_Verts3D.append(pos)
+                                                    
                 #BBOX COLLECT
                 for v in bbox_Verts3D:                
                     pos3D = v            
                     pos2D = location_3d_to_region_2d(region, rv3d, pos3D, default=None)
                     bboxBatch3D.append(pos3D)
-                    bboxBatch.append(pos2D)                
+                    bboxBatch.append(pos2D)
+                             
                 #print (len(bboxBatch))            
             
             #bpy.data.meshes.remove(bbox_mesh, do_unlink=True,do_id_user=True, do_ui_user=True)                
@@ -222,6 +306,8 @@ class IOPS_OP_PlaceOrigin(bpy.types.Operator):
 #                faceBatch3D.append(pos3D)
 #                faceBatch.append(pos2D)
             #return vertsPos            
+            self.posBatch = bboxBatch
+            self.posBatch3D = bboxBatch3D  
             return [faceBatch,faceBatch3D, bboxBatch, bboxBatch3D]
         else:
             return [None,None, bboxBatch, bboxBatch3D]
@@ -235,9 +321,17 @@ class IOPS_OP_PlaceOrigin(bpy.types.Operator):
         elif event.type == 'MOUSEMOVE':
             self.mouse_pos = event.mouse_region_x, event.mouse_region_y
             self.SceneRayCast(context)
-            self.ObjectBBox(context)          
+            self.ObjectBBox(context)
+            self.calc_distance(context)          
 #            print ("VERT POS",self.getFaceVertPos(context))
 #            print ("MOUSE POS",self.mouse_pos)
+        elif event.type in {"LEFTMOUSE", "SPACE"}:
+            self.PlaceOrigin(context)
+            bpy.types.SpaceView3D.draw_handler_remove(self._handle_points, "WINDOW")
+            bpy.types.SpaceView3D.draw_handler_remove(self._handle_bbox_lines, "WINDOW")
+            bpy.types.SpaceView3D.draw_handler_remove(self._handle_bbox_points,'WINDOW')
+            bpy.types.SpaceView3D.draw_handler_remove(self._handle_bbox_act_point,'WINDOW')
+            return {"FINISHED"}
             
             
         elif event.type in {'RIGHTMOUSE', 'ESC'}:
@@ -247,6 +341,7 @@ class IOPS_OP_PlaceOrigin(bpy.types.Operator):
             bpy.types.SpaceView3D.draw_handler_remove(self._handle_points, "WINDOW")
             bpy.types.SpaceView3D.draw_handler_remove(self._handle_bbox_lines, "WINDOW")
             bpy.types.SpaceView3D.draw_handler_remove(self._handle_bbox_points,'WINDOW')
+            bpy.types.SpaceView3D.draw_handler_remove(self._handle_bbox_act_point,'WINDOW')
             return {'CANCELLED'}
 
         return {'RUNNING_MODAL'}
@@ -260,6 +355,7 @@ class IOPS_OP_PlaceOrigin(bpy.types.Operator):
             self._handle_points = bpy.types.SpaceView3D.draw_handler_add(draw_multicircles_fill_2d_Face,args,'WINDOW','POST_PIXEL')
             self._handle_bbox_lines = bpy.types.SpaceView3D.draw_handler_add(draw_bbox_lines,args,'WINDOW','POST_VIEW')
             self._handle_bbox_points = bpy.types.SpaceView3D.draw_handler_add(draw_multicircles_fill_2d_BBOX,args,'WINDOW','POST_PIXEL')
+            self._handle_bbox_act_point = bpy.types.SpaceView3D.draw_handler_add(draw_circle_fill_2d,args,'WINDOW','POST_PIXEL')
             
             print("----------------------------------------")
             
