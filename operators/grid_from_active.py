f99a73852ebb77f8379fa971a57338c2e1263525
Furash <cg.furash@gmail.com>
2020-03-16 18:54:44 +0100

    precision/debug for grid from active

--- operators/grid_from_active.py
+++ operators/grid_from_active.py
@@ -35,12 +35,20 @@ class IOPS_OT_ToGridFromActive(bpy.types.Operator):
         o_xz = np.array([origin[0], origin[2]])
 
         # Move objects to grid      
-        for o in objects:
-            p = np.array([o.location[0], o.location[2]])
-            p_prime = np.round((p - o_xz) / (size_x, size_z)) * (size_x, size_z) + o_xz
-            location = Vector((p_prime[0], origin[1], p_prime[1]))
-            o.matrix_world @= o.matrix_world.inverted()
+        for o in objects: 
+            print("Location difference before:", o.location - active.location)
+            p = np.array([o.location[0], o.location[2]]) 
+            p_prime = np.around((p - o_xz) / (size_x, size_z)) * (size_x, size_z) + o_xz 
+            if np.allclose(p, p_prime, rtol=1e-21, atol=1e-24): 
+                continue 
+            location = Vector((p_prime[0], origin[1], p_prime[1])) 
+            o.matrix_world @= o.matrix_world.inverted() 
             o.location = location
+            print("Location difference after:", o.location - active.location)
+
+            # print("----")
+            # print('p', p)
+            # print('p_prime', p_prime)
 
         # Restore matrix for all and clear parent    
         bpy.ops.object.parent_set(type='OBJECT', keep_transform=True)
