96fe299692ca5b70344fdbcc67a1c3fb69603f7c
Furash <cg.furash@gmail.com>
2020-04-25 23:51:24 +0200

    connected pies to refactor

--- operators/split_screen_area.py
+++ operators/split_screen_area.py
@@ -1,6 +1,5 @@
 import bpy
 from bpy.props import StringProperty, FloatProperty
-from .. prefs.split_area_list import split_area_list
 import copy
 
 def ContextOverride(area):
@@ -107,347 +106,6 @@ def join_area_bottom(join_x, join_y, area):
         return "Cannot join area"
 
 
-
-class IOPS_OT_SplitAreaUV(bpy.types.Operator):
-    bl_idname = "iops.split_area_uv"
-    bl_label = "IOPS Split Area UV"
-
-    def execute(self,context):
-        current_area = context.area
-        override = current_area
-        current_screen =  bpy.context.screen
-        side_area = None
-        join_x = current_area.x + current_area.width + 1
-        join_y = int(current_area.y + current_area.height/2)
-        areas = list(context.screen.areas)
-
-        # Check if toggle fullscreen was activated
-        if "nonnormal" in current_screen.name: 
-            bpy.ops.screen.screen_full_area(use_hide_panels=True)
-            return {"FINISHED"}
-
-        for area in context.screen.areas:
-            if area == current_area:
-                continue
-            elif area.x == join_x and area.y == current_area.y:
-                side_area = area
-                break
-
-        if side_area and side_area.type == 'IMAGE_EDITOR':
-            event = join_area_right(join_x, join_y, side_area)
-            self.report({"INFO"}, event)
-            return {"FINISHED"}
-        
-        if current_area.type == 'IMAGE_EDITOR':
-            join_x = current_area.x - 1
-            join_y = int(current_area.height/2 + current_area.y)
-            override = get_neighbour_left(current_area)
-            event = join_area_right(join_x, join_y, override)
-            self.report({"INFO"}, event)
-            return {"FINISHED"}
-        
-        else:
-            context.area.type = current_area.type
-            new_area = None
-            bpy.ops.screen.area_split(direction="VERTICAL", factor=0.51)
-            for area in context.screen.areas:
-                if area not in areas:
-                    new_area = area
-                    break
-            if new_area:
-                new_area.type = 'IMAGE_EDITOR'
-                new_area.ui_type = 'UV'
-                return {"FINISHED"}
-        
-        return {"CANCELLED"}
-
-
-class IOPS_OT_SplitAreaOutliner(bpy.types.Operator):
-    bl_idname = "iops.split_area_outliner"
-    bl_label = "IOPS Split Area Outliner"
-
-    def execute(self,context):
-        current_area = context.area
-        override = current_area
-        current_screen =  bpy.context.screen
-        side_area = None
-        join_x = current_area.x
-        join_y = int(current_area.y + current_area.height/2)
-        areas = list(context.screen.areas)
-
-        # Check if toggle fullscreen was activated
-        if "nonnormal" in current_screen.name: 
-            bpy.ops.screen.screen_full_area(use_hide_panels=True)
-            return {"FINISHED"}
-
-        for area in context.screen.areas:
-            if area == current_area:
-                continue
-            elif area.width + area.x + 1 == join_x and area.y == current_area.y:
-                side_area = area
-                break
-
-        if side_area and side_area.type == 'OUTLINER':
-            event = join_area_left(join_x, join_y, side_area)
-            self.report({"INFO"}, event)
-            return {"FINISHED"}
-        
-        if current_area.type == 'OUTLINER':
-            join_x = current_area.width + current_area.x + 1
-            override = get_neighbour_right(current_area)
-            if override:
-                event = join_area_left(join_x, join_y, override)
-                self.report({"INFO"}, event)
-                return {"FINISHED"}
-            elif override is None:
-                override = get_neighbour_left(current_area)
-                join_x = current_area.x + 1
-                event = join_area_right(join_x, join_y, override)
-                self.report({"INFO"}, event)
-                return {"FINISHED"}
-            else:
-                self.report({"INFO"}, "Cannot get neighbour area") 
-        
-        else:
-            context.area.type = current_area.type
-            new_area = None
-            bpy.ops.screen.area_split(direction="VERTICAL", factor=0.15)
-
-            for area in context.screen.areas:
-                if area not in areas:
-                    new_area = area
-                    break
-            if new_area:
-                new_area.type = "OUTLINER" # VIEW_3D
-                return {"FINISHED"}
-        
-        return {"CANCELLED"}
-
-class IOPS_OT_SplitAreaProperties(bpy.types.Operator):
-    bl_idname = "iops.split_area_properties"
-    bl_label = "IOPS Split Area Properties"
-
-    def execute(self,context):
-        current_area = context.area
-        override = current_area
-        current_screen =  bpy.context.screen
-        side_area = None
-        join_x = current_area.x + current_area.width + 1
-        join_y = int(current_area.y + current_area.height/2)
-        areas = list(context.screen.areas)
-
-        # Check if toggle fullscreen was activated
-        if "nonnormal" in current_screen.name: 
-            # bpy.ops.screen.back_to_previous()
-            bpy.ops.screen.screen_full_area(use_hide_panels=True)
-            return {"FINISHED"}
-
-        for area in context.screen.areas:
-            if area == current_area:
-                continue
-            elif area.x == join_x and area.y == current_area.y:
-                side_area = area
-                break
-
-        if side_area and side_area.type == 'PROPERTIES':
-            event = join_area_right(join_x, join_y, side_area)
-            self.report({"INFO"}, event)
-            return {"FINISHED"}
-        
-        if current_area.type == 'PROPERTIES':
-            join_x = current_area.x - 1
-            join_y = int(current_area.height/2 + current_area.y)
-            override = get_neighbour_left(current_area)
-            event = join_area_right(join_x, join_y, override)
-            self.report({"INFO"}, event)
-            return {"FINISHED"}
-        
-        else:
-            context.area.type = current_area.type
-            new_area = None
-            bpy.ops.screen.area_split(direction="VERTICAL", factor=0.8)
-            for area in context.screen.areas:
-                if area not in areas:
-                    new_area = area
-                    break
-            if new_area:
-                new_area.type = 'PROPERTIES' 
-                return {"FINISHED"}
-        
-        return {"CANCELLED"}
-
-
-class IOPS_OT_SplitAreaText(bpy.types.Operator):
-    bl_idname = "iops.split_area_text"
-    bl_label = "IOPS Split Area Text"
-
-
-    def execute(self,context):
-        current_area = context.area
-        current_screen =  bpy.context.screen
-        override = current_area
-        side_area = None
-        join_x = current_area.x + current_area.width + 1
-        join_y = int(current_area.y + current_area.height/2)
-        areas = list(context.screen.areas)
-
-        # Check if toggle fullscreen was activated
-        if "nonnormal" in current_screen.name: 
-            bpy.ops.screen.screen_full_area(use_hide_panels=True)
-            return {"FINISHED"}
-
-        for area in context.screen.areas:
-            if area == current_area:
-                continue
-            elif area.x == join_x and area.y == current_area.y:
-                side_area = area
-                break
-
-        if side_area and side_area.type == 'TEXT_EDITOR':
-            event = join_area_right(join_x, join_y, side_area)
-            self.report({"INFO"}, event)
-            return {"FINISHED"}
-        
-        if current_area.type == 'TEXT_EDITOR':
-            join_x = current_area.x - 1
-            join_y = int(current_area.height/2 + current_area.y)
-            override = get_neighbour_left(current_area)
-            event = join_area_right(join_x, join_y, override)
-            self.report({"INFO"}, event)
-            return {"FINISHED"}
-        
-        else:
-            context.area.type = current_area.type
-            new_area = None
-            bpy.ops.screen.area_split(direction="VERTICAL", factor=0.8)
-            for area in context.screen.areas:
-                if area not in areas:
-                    new_area = area
-                    break
-            if new_area:
-                new_area.type = 'TEXT_EDITOR' 
-                return {"FINISHED"}
-        
-        return {"CANCELLED"}
-
-
-class IOPS_OT_SplitAreaConsole(bpy.types.Operator):
-    bl_idname = "iops.split_area_console"
-    bl_label = "IOPS Split Area Console"
-
-
-    def execute(self,context):
-        current_area = context.area
-        current_screen =  bpy.context.screen
-        override = current_area
-        current_area_top = current_area.height + current_area.y + 1
-        side_area = None
-        join_x = int(current_area.x + current_area.width / 2)
-        join_y = current_area_top
-        areas = list(context.screen.areas)
-
-        # Check if toggle fullscreen was activated
-        if "nonnormal" in current_screen.name: 
-            bpy.ops.screen.screen_full_area(use_hide_panels=True)
-
-            return {"FINISHED"}
-
-        for area in context.screen.areas:
-            if area == current_area:
-                continue
-            elif area.x == current_area.x and area.y == current_area_top:
-                side_area = area
-                break
-
-        if side_area and side_area.type == 'CONSOLE':
-            event = join_area_top(join_x, join_y, side_area)
-            self.report({"INFO"}, event)
-
-            return {"FINISHED"}
-        
-        if current_area.type == 'CONSOLE':
-            join_x = int(current_area.width/2 + current_area.x)
-            join_y = current_area.y - 1
-            override = get_neighbour_bottom(current_area)
-            event = join_area_top(join_x, join_y, override)
-            self.report({"INFO"}, event)
-
-            return {"FINISHED"}
-        
-        else:
-            context.area.type = current_area.type
-            new_area = None
-            bpy.ops.screen.area_split(direction="HORIZONTAL", factor=0.8)
-            for area in context.screen.areas:
-                if area not in areas:
-                    new_area = area
-                    break
-
-            if new_area:
-                new_area.type = 'CONSOLE' 
-                return {"FINISHED"}
-
-        return {"CANCELLED"}
-
-
-class IOPS_OT_SplitAreaTimeline(bpy.types.Operator):
-    bl_idname = "iops.split_area_timeline"
-    bl_label = "IOPS Split Area Timeline"
-
-
-    def execute(self,context):
-        current_area = context.area
-        current_screen =  bpy.context.screen
-        override = current_area
-        side_area = None
-        join_x = int(current_area.x + current_area.width / 2)
-        join_y = current_area.y
-        areas = list(context.screen.areas)
-
-        # Check if toggle fullscreen was activated
-        if "nonnormal" in current_screen.name: 
-            bpy.ops.screen.screen_full_area(use_hide_panels=True)
-
-            return {"FINISHED"}
-
-        for area in context.screen.areas:
-            if area == current_area:
-                continue
-            elif area.x == current_area.x and area.height + area.y + 1 == current_area.y:
-                side_area = area
-                break
-
-        if side_area and side_area.type == 'DOPESHEET_EDITOR':
-            event = join_area_bottom(join_x, join_y, side_area)
-            self.report({"INFO"}, event)
-
-            return {"FINISHED"}
-        
-        if current_area.type == 'DOPESHEET_EDITOR':
-            join_x = int(current_area.width/2 + current_area.x)
-            join_y = current_area.height + current_area.y + 1
-            override = get_neighbour_top(current_area)
-            event = join_area_bottom(join_x, join_y, override)
-            self.report({"INFO"}, event)
-
-            return {"FINISHED"}
-        
-        else:
-            context.area.type = current_area.type
-            new_area = None
-            bpy.ops.screen.area_split(direction="HORIZONTAL", factor=0.2)
-            for area in context.screen.areas:
-                if area not in areas:
-                    new_area = area
-                    break
-
-            if new_area:
-                new_area.type = 'DOPESHEET_EDITOR' 
-                new_area.ui_type = 'TIMELINE'
-                return {"FINISHED"}
-
-        return {"CANCELLED"}    
-
 # ########################################################################################
 #                                     REFACTORING
 # ########################################################################################
@@ -457,7 +115,6 @@ class IOPS_OT_SplitScreenArea(bpy.types.Operator):
     bl_idname = "iops.split_screen_area"
     bl_label = "IOPS Split Screen Area"
 
-# StringProperty(name="", description="", default="", maxlen=0, options={'ANIMATABLE'}, tags={}, subtype='NONE', update=None, get=None, set=None)
 
     area : StringProperty(
         name="Area",
@@ -465,19 +122,20 @@ class IOPS_OT_SplitScreenArea(bpy.types.Operator):
         default=""
     )
 
-    direction : StringProperty(
-        name="Direction",
+    pos : StringProperty(
+        name="Position",
         description="Where to create new area",
         default=""
     )
 
-    factor: FloatProperty(
+    factor : FloatProperty(
         name="Factor",
         description="Area split factor",
         default=0.01,
         soft_min=0.01,
         soft_max=1,
-        step=0.01
+        step=0.01,
+        precision=2
     )        
 
     
@@ -508,51 +166,51 @@ class IOPS_OT_SplitScreenArea(bpy.types.Operator):
 
 
     def execute(self, context):
-        current_area = context.area
-        side_area = None
-        join_x, join_y = self.get_join_xy(context, area, direction)
-        areas = list(context.screen.areas)
+        # current_area = context.area
+        # side_area = None
+        # join_x, join_y = self.get_join_xy(context, area, direction)
+        # areas = list(context.screen.areas)
 
-        # Check if toggle fullscreen was activated
-        if "nonnormal" in context.screen.name: 
-            bpy.ops.screen.screen_full_area(use_hide_panels=True)
+        # # Check if toggle fullscreen was activated
+        # if "nonnormal" in context.screen.name: 
+        #     bpy.ops.screen.screen_full_area(use_hide_panels=True)
 
-            return {"FINISHED"}
+        #     return {"FINISHED"}
 
-        for area in context.screen.areas:
-            if area == current_area:
-                continue
-            elif area.x == current_area.x and area.height + area.y + 1 == current_area.y:
-                side_area = area
-                break
+        # for area in context.screen.areas:
+        #     if area == current_area:
+        #         continue
+        #     elif area.x == current_area.x and area.height + area.y + 1 == current_area.y:
+        #         side_area = area
+        #         break
 
-        if side_area and side_area.type == 'DOPESHEET_EDITOR':
-            event = join_area_bottom(join_x, join_y, side_area)
-            self.report({"INFO"}, event)
+        # if side_area and side_area.type == 'DOPESHEET_EDITOR':
+        #     event = join_area_bottom(join_x, join_y, side_area)
+        #     self.report({"INFO"}, event)
 
-            return {"FINISHED"}
+        #     return {"FINISHED"}
         
-        if current_area.type == 'DOPESHEET_EDITOR':
-            join_x = int(current_area.width/2 + current_area.x)
-            join_y = current_area.height + current_area.y + 1
-            override = get_neighbour_top(current_area)
-            event = join_area_bottom(join_x, join_y, override)
-            self.report({"INFO"}, event)
+        # if current_area.type == 'DOPESHEET_EDITOR':
+        #     join_x = int(current_area.width/2 + current_area.x)
+        #     join_y = current_area.height + current_area.y + 1
+        #     override = get_neighbour_top(current_area)
+        #     event = join_area_bottom(join_x, join_y, override)
+        #     self.report({"INFO"}, event)
 
-            return {"FINISHED"}
+        #     return {"FINISHED"}
         
-        else:
-            context.area.type = current_area.type
-            new_area = None
-            bpy.ops.screen.area_split(direction="HORIZONTAL", factor=0.2)
-            for area in context.screen.areas:
-                if area not in areas:
-                    new_area = area
-                    break
+        # else:
+        #     context.area.type = current_area.type
+        #     new_area = None
+        #     bpy.ops.screen.area_split(direction="HORIZONTAL", factor=0.2)
+        #     for area in context.screen.areas:
+        #         if area not in areas:
+        #             new_area = area
+        #             break
 
-            if new_area:
-                new_area.type = 'DOPESHEET_EDITOR' 
-                new_area.ui_type = 'TIMELINE'
-                return {"FINISHED"}
-
-        return {"CANCELLED"}
+        #     if new_area:
+        #         new_area.type = 'DOPESHEET_EDITOR' 
+        #         new_area.ui_type = 'TIMELINE'
+        #         return {"FINISHED"}
+        self.report({'INFO'},f'Area: {self.area}, Pos: {self.pos}, Factor: {self.factor}')
+        return {"FINISHED"}
