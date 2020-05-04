51babc7eb4e7e76d3d251b7b9718f3d6f5dbdf30
TitusLVR <Titus.mailbox@gmail.com>
2020-05-04 21:20:10 +0200

    Quick snap - update

--- operators/quick_snap.py
+++ operators/quick_snap.py
@@ -10,14 +10,6 @@ class IOPS_OT_QuickSnap(bpy.types.Operator):
     bl_label = "IOPS Quick Snap"
     bl_options = {"REGISTER", "UNDO"}
 
-    quick_snap_diff: FloatProperty(
-        name="", 
-        description="Guess Value", 
-        default=0.5, 
-        min=0.0001,
-        max=1000,
-    )
-
     # @classmethod 
     # def poll(cls, context):
     #     return (and context.area.type == "VIEW_3D")
@@ -32,11 +24,9 @@ class IOPS_OT_QuickSnap(bpy.types.Operator):
             # GET INDEXES
             bm = bmesh.from_edit_mesh(me)
             selected_verts_index = []
-            # print("____________________________________________")
             for v in bm.verts:
                 if v.select:
-                    selected_verts_index.append(v.index)        
-                    # print ("VERTS INDEX:", v.index)
+                    selected_verts_index.append(v.index)
             bpy.ops.object.editmode_toggle()
             bm.free()
             bpy.ops.object.editmode_toggle()
@@ -58,26 +48,15 @@ class IOPS_OT_QuickSnap(bpy.types.Operator):
                     local_pos = ob_mw_i @ v1  # face cent in sphere local space
                     (hit, loc, norm, face_index) = ob.closest_point_on_mesh(local_pos)
                     if hit:
-                        v2 = ob.matrix_world @ loc
-                        c_dist = (v2 - v1).length
-            #            print(ob.name, (v2 - v1).length)
-                        # print("Closest dist:",c_dist)
                         bm.verts.ensure_lookup_table()
                         bm.faces.ensure_lookup_table()
                         v_dists = {}
                         for v in ob.data.polygons[face_index].vertices:                                
-                            v_co = ob.matrix_world @ ob.data.vertices[v].co 
-            #                target_points.append(v_co)                          
-                            # v_dist = (v_co - v1).length - self.quick_snap_diff
-                            # print("Vertex dist:",v_dist)                             
-                            # if v_dist <= c_dist:
-                            #     target_points.append([ind,v_co])
+                            v_co = ob.matrix_world @ ob.data.vertices[v].co                 
                             v_dist = (v_co - v1).length 
                             v_dists[v] = {}
                             v_dists[v]["co"] = (*v_co,)
                             v_dists[v]["len"] = v_dist
-                            print (json.dumps(v_dists, indent=4))
-                        
                         
                         lens = [v_dists[idx]["len"] for idx in v_dists]
                         for k in v_dists.values():    
@@ -87,21 +66,12 @@ class IOPS_OT_QuickSnap(bpy.types.Operator):
                         target_points.append([ind, Vector(min_co)])
                                                    
                 bm.clear()
-                
-
-            # print (target_points[0][0])
 
             bm = bmesh.from_edit_mesh(me)
             bm.verts.ensure_lookup_table()
             for p in target_points:
                 bm.verts[p[0]].co = edit_obj.matrix_world.inverted() @ p[1]
             bmesh.update_edit_mesh(me)
-
-
-            #bpy.ops.object.editmode_toggle()
-            #for p in target_points: 
-            #    print ("------", p)   
-            #    bpy.ops.object.empty_add(location=p)
 
             self.report({'INFO'}, "POINTS ARE SNAPPED!!!")
         else:
