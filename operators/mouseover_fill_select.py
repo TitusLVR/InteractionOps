0b20fb9ae966428f265012344efb3b40d8873594
Furash <cg.furash@gmail.com>
2021-03-12 11:21:26 +0000

    undo added to fill select

--- operators/mouseover_fill_select.py
+++ operators/mouseover_fill_select.py
@@ -1,10 +1,11 @@
 import bpy
-# import bmesh
+
 
 class IOPS_MouseoverFillSelect(bpy.types.Operator):
     """Fill select faces at mouseover"""
     bl_idname = "iops.mouseover_fill_select"
     bl_label = "IOPS Mouseover Fill Select"
+    bl_options = {"REGISTER", "UNDO"}  
 
     @classmethod
     def poll(cls, context):
@@ -13,26 +14,9 @@ class IOPS_MouseoverFillSelect(bpy.types.Operator):
                 context.object.data.is_editmode)
 
     def invoke(self, context, event):
-        # me = context.object.data
-        # bm = bmesh.from_edit_mesh(me)
-        # verts_sel = [v.select for v in bm.verts]
-        # edges_sel = [e.select for e in bm.edges]
-        # faces_sel = [f.select for f in bm.faces]
-
         loc = event.mouse_region_x, event.mouse_region_y
-
         bpy.ops.mesh.hide(unselected=False)
-        ret = bpy.ops.view3d.select(extend=True, location=loc)
-        if ret == {'PASS_THROUGH'}:
-            self.report({'INFO'}, "No geometry under cursor!")
-            return {'CANCELLED'}
-        
+        bpy.ops.view3d.select(extend=True, location=loc) 
         bpy.ops.mesh.select_linked(delimit={'NORMAL'})
         bpy.ops.mesh.reveal(select=True)
-
-        # try:
-        #     geom = bm.select_history[-1]
-        # except IndexError:
-        #     geom = None
-
         return {'FINISHED'}
