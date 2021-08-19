f9cb5adb28918d3d5d27bc1635a6b0e5b46ab1bd
TitusLVR <Titus.mailbox@gmail.com>
2021-08-19 17:15:49 +0200

    Udated AlignBetween OP

--- operators/align_between_two.py
+++ operators/align_between_two.py
@@ -10,7 +10,7 @@ from bpy.props import (BoolProperty,
                        FloatVectorProperty,
                        )
 
-from ..utils.functions import get_active_and_selected
+from ..utils.functions import get_active_and_selected, ShowMessageBox
 
 
 class IOPS_OT_Align_between_two(bpy.types.Operator):
@@ -18,7 +18,7 @@ class IOPS_OT_Align_between_two(bpy.types.Operator):
     bl_idname = "iops.align_between_two"
     bl_label = "Align Between Two"
     bl_options = {'REGISTER', 'UNDO'}
-
+    
     track_axis: EnumProperty(
         name='Track',
         description='Track axis',
@@ -63,67 +63,59 @@ class IOPS_OT_Align_between_two(bpy.types.Operator):
         )
 
     def align_between(self):
-        sequence = []
-        active, objects = get_active_and_selected()
-        if len(bpy.context.selected_objects) == 2:
-            axis = active.location - objects[-1].location
-            
-            A = active.location 
-            B = objects[-1].location 
-
-            for ip in range(self.count):
-                p = 1 / (self.count + 1) * (ip + 1)
-                point = ((1 - p)  * A + p * B)
-                sequence.append(point)
-
-        elif len(bpy.context.selected_objects) == 3:
-            posA, posB = [ob.location for ob in objects]
-            axis = posA - posB
-            
-            for idx in range(len(objects) - 1):
-                A = objects[idx].location 
-                B = objects[idx + 1].location 
+            sequence = []
+            active, objects = get_active_and_selected()
+            if len(bpy.context.selected_objects) == 2:
+                axis = active.location - objects[-1].location
+                
+                A = active.location 
+                B = objects[-1].location 
 
                 for ip in range(self.count):
                     p = 1 / (self.count + 1) * (ip + 1)
                     point = ((1 - p)  * A + p * B)
                     sequence.append(point)
+            elif len(bpy.context.selected_objects) == 3:
+                posA, posB = [ob.location for ob in objects]
+                axis = posA - posB
+                
+                for idx in range(len(objects) - 1):
+                    A = objects[idx].location 
+                    B = objects[idx + 1].location 
 
-        collection = bpy.data.collections.new("Objects Between")
-        bpy.context.scene.collection.children.link(collection)
-        new_objects = []
-        for p in sequence:
-            new_ob = active.copy()
-            new_ob.data = active.data.copy()
-            # position
-            new_ob.location = p 
-            # rotation
-            if self.align:                    
-                new_ob.rotation_mode = 'QUATERNION'
-                new_ob.rotation_quaternion = axis.to_track_quat(self.track_axis, self.up_axis)
-                new_ob.rotation_mode = 'XYZ'
-            
-            collection.objects.link(new_ob)
-            new_ob.select_set(False)
-            new_objects.append(new_ob)
-                    
-        if self.select_duplicated:
-            active.select_set(False)
-            for ob in objects:
-                ob.select_set(False)
-            for ob in new_objects:
-                ob.select_set(True)
-            bpy.context.view_layer.objects.active = new_objects[-1] 
-        
-        else:
-            return
-    
-    @classmethod
-    def poll(self, context):
-        return (context.area.type == "VIEW_3D" and
-                context.mode == "OBJECT" and
-                len(context.view_layer.objects.selected) == 2 or
-                len(context.view_layer.objects.selected) == 3)
+                    for ip in range(self.count):
+                        p = 1 / (self.count + 1) * (ip + 1)
+                        point = ((1 - p)  * A + p * B)
+                        sequence.append(point)
+            else:
+                ShowMessageBox("Must be 2 or 3 Objects Selected.")
+                return
+
+            collection = bpy.data.collections.new("Objects Between")
+            bpy.context.scene.collection.children.link(collection)
+            new_objects = []
+            for p in sequence:
+                new_ob = active.copy()
+                new_ob.data = active.data.copy()
+                # position
+                new_ob.location = p 
+                # rotation
+                if self.align:                    
+                    new_ob.rotation_mode = 'QUATERNION'
+                    new_ob.rotation_quaternion = axis.to_track_quat(self.track_axis, self.up_axis)
+                    new_ob.rotation_mode = 'XYZ'
+                
+                collection.objects.link(new_ob)
+                new_ob.select_set(False)
+                new_objects.append(new_ob)
+                        
+            if self.select_duplicated:
+                active.select_set(False)
+                for ob in objects:
+                    ob.select_set(False)
+                for ob in new_objects:
+                    ob.select_set(True)
+                bpy.context.view_layer.objects.active = new_objects[-1] 
     
     def execute(self, context):
         if self.track_axis != self.up_axis:
@@ -137,7 +129,9 @@ class IOPS_OT_Align_between_two(bpy.types.Operator):
     def draw(self, context):
             layout = self.layout
             col = layout.column(align=True) 
-            # col.prop(self, "use_active_collection")
+            #
+            col.prop(self, "mode")
+            col.separator()
             col.prop(self, "track_axis")
             col.prop(self, "up_axis")
             col.separator()
