ff47d20ef149202d616f60c8767e6e9322094b94
Cyrill Vitkovskiy <4040177+Furash@users.noreply.github.com>
2023-04-23 16:40:00 +0100

    set vertex color works on multiple objects now

--- operators/assign_vertex_color.py
+++ operators/assign_vertex_color.py
@@ -12,35 +12,43 @@ class IOPS_OT_VertexColorAssign(bpy.types.Operator):
         return context.object.type == 'MESH'
 
     def execute(self, context):
-        if context.object.type == "MESH":
-            if context.mode == 'OBJECT':
-                me = context.object.data
-                if me.vertex_colors[:] == []:
-                    me.vertex_colors.new()
-                vcol_data = me.vertex_colors.active.data
-                if vcol_data:
-                    for p in me.polygons:
-                        color = context.tool_settings.image_paint.brush.color                                
-                        for loop_index in p.loop_indices:
-                            vcol_data[loop_index].color = (color[0],color[1],color[2], 1.0)                
-                    me.update()        
-            
-            if context.mode == 'EDIT_MESH':
-                tool_mesh = bpy.context.scene.tool_settings.mesh_select_mode
-                vertex = tool_mesh[0]
-                face = tool_mesh[2]
-                bpy.context.tool_settings.vertex_paint.brush.color = context.tool_settings.image_paint.brush.color
-                bpy.ops.object.mode_set(mode = 'VERTEX_PAINT')
-                if vertex:            
-                    bpy.context.object.data.use_paint_mask_vertex = True
-                    bpy.ops.paint.vertex_color_set()
-                    bpy.ops.object.mode_set(mode = 'EDIT')
-                if face:
-                    bpy.context.object.data.use_paint_mask = True
-                    bpy.ops.paint.vertex_color_set()
-                    bpy.ops.object.mode_set(mode = 'EDIT')
-        else:
-            self.report({'WARNING'}, "Not a MESH")
+        sel = [obj for obj in context.selected_objects]
+        bpy.ops.object.select_all(action='DESELECT')
+
+        for obj in sel:
+            obj.select_set(True)
+            context.view_layer.objects.active = obj
+            if obj.type == "MESH":
+                if context.mode == 'OBJECT':
+                    me = obj.data
+                    if me.vertex_colors[:] == []:
+                        me.vertex_colors.new()
+                    vcol_data = me.vertex_colors.active.data
+                    if vcol_data:
+                        for p in me.polygons:
+                            color = context.tool_settings.image_paint.brush.color                                
+                            for loop_index in p.loop_indices:
+                                vcol_data[loop_index].color = (color[0],color[1],color[2], 1.0)                
+                        me.update()        
+                
+                if context.mode == 'EDIT_MESH':
+                    tool_mesh = context.scene.tool_settings.mesh_select_mode
+                    vertex = tool_mesh[0]
+                    face = tool_mesh[2]
+                    context.tool_settings.vertex_paint.brush.color = context.tool_settings.image_paint.brush.color
+                    bpy.ops.object.mode_set(mode = 'VERTEX_PAINT')
+                    if vertex:            
+                        context.object.data.use_paint_mask_vertex = True
+                        bpy.ops.paint.vertex_color_set()
+                        bpy.ops.object.mode_set(mode = 'EDIT')
+                    if face:
+                        context.object.data.use_paint_mask = True
+                        bpy.ops.paint.vertex_color_set()
+                        bpy.ops.object.mode_set(mode = 'EDIT')
+            else:
+                self.report({'WARNING'}, obj.name + " is not a MESH.")
+            obj.select_set(False)
+
         return {'FINISHED'}
     
     def draw(self, context):
