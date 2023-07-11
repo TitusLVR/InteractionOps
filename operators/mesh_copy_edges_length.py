c430d4b717e4de33e50ea415a37a9bf9578d82dc
Cyrill Vitkovskiy <cyrillv@ccpgames.com>
2023-07-11 16:28:00 +0100

    Update mesh_copy_edges_length.py
    Added unit scale fix to edge length copy

--- operators/mesh_copy_edges_length.py
+++ operators/mesh_copy_edges_length.py
@@ -1,6 +1,24 @@
 import bpy
 import bmesh
 
+def get_unit_scale():
+    """Get the unit scale based on the current scene units"""
+    units = bpy.data.scenes["Scene"].unit_settings.length_unit
+    if units == 'MICROMETERS':
+        return 0.000001
+    elif units == 'MILLIMETERS':
+        return 0.001
+    elif units == 'CENTIMETERS':
+        return 0.01
+    elif units == 'METERS':
+        return 1.0
+    elif units == 'KILOMETERS':
+        return 1000.0
+    elif units == 'ADAPTIVE':
+        return 1.0
+    else:
+        return 1.0
+
 class IOPS_MESH_OT_CopyEdgesLength(bpy.types.Operator):
     """Copy the active edge length to the clipboard"""
     bl_idname = "mesh.iops_copy_edge_length"
@@ -21,7 +39,7 @@ class IOPS_MESH_OT_CopyEdgesLength(bpy.types.Operator):
 
         if selected_edges:
             # Add the length of all selected edges and store it in the clipboard
-            total_length = sum(e.calc_length() for e in selected_edges)
+            total_length = sum(e.calc_length() for e in selected_edges) / get_unit_scale()
             bpy.context.window_manager.clipboard = str(total_length)
             self.report({'INFO'}, "Copied the sum of edges' length to clipboard")
             bm.free()            
