1edcbac754750bf416554f60c348872f449b418a
Cyrill Vitkovskiy <cyrillv@ccpgames.com>
2023-08-23 12:02:18 +0100

    GLOBAL orientation for drag_snap_cursor

--- operators/drag_snap_cursor.py
+++ operators/drag_snap_cursor.py
@@ -27,12 +27,12 @@ class IOPS_OT_DragSnapCursor(bpy.types.Operator):
 
     def modal(self, context, event):
         #prevent spamming
-        new_type = event.type
-        new_value = event.value
-        if new_type != self.old_type and new_value != self.old_value:
-            print(event.type, event.value)
-            self.old_type = new_type
-            self.old_value = new_value
+        # new_type = event.type
+        # new_value = event.value
+        # if new_type != self.old_type and new_value != self.old_value:
+        #     print(event.type, event.value)
+        #     self.old_type = new_type
+        #     self.old_value = new_value
 
         if event.type in {'MIDDLEMOUSE', 'WHEELUPMOUSE', 'WHEELDOWNMOUSE'} and event.value == 'PRESS':
             return {'PASS_THROUGH'}
@@ -62,7 +62,7 @@ class IOPS_OT_DragSnapCursor(bpy.types.Operator):
                 # print("Count:", 3)
                 bpy.context.scene.IOPS.dragsnap_point_b = bpy.context.scene.cursor.location
                 vector = bpy.context.scene.IOPS.dragsnap_point_b - bpy.context.scene.IOPS.dragsnap_point_a
-                bpy.ops.transform.translate(value=vector)
+                bpy.ops.transform.translate(value=vector, orient_type='GLOBAL')
                 return {'FINISHED'}
 
         return {'RUNNING_MODAL'}
