2e5aca56064f638c132a5eb10d64ab8765c75b57
Titus Lavrov <titus.mailbox@gmail.com>
2023-12-05 15:11:01 +0100

    Update grid_from_active.py
    update to XYZ grid

--- operators/grid_from_active.py
+++ operators/grid_from_active.py
@@ -25,45 +25,41 @@ class IOPS_OT_ToGridFromActive(bpy.types.Operator):
         # Reset transforms of all
         bpy.ops.object.parent_set(type='OBJECT', keep_transform=True)
         active.matrix_world @= active.matrix_world.inverted()
-        bpy.ops.object.parent_clear(type='CLEAR_KEEP_TRANSFORM')                
+        bpy.ops.object.parent_clear(type='CLEAR_KEEP_TRANSFORM')
 
         # Sizes
         size_x = active.dimensions[0]
         size_y = active.dimensions[1]
         size_z = active.dimensions[2]
 
-        o_xz = np.array([origin[0], origin[2]], dtype=np.float32)
         # Move objects to grid
-        # for o in objects:
-        #     p = np.array([o.location[0], o.location[2]], dtype=np.float32)
-        #     p_prime = np.around((p - o_xz) / (size_x, size_z)) * (size_x, size_z) + o_xz
-        #     if np.allclose(p, p_prime):
-        #         print(f'Skipping p={p}, pp={p_prime}, p-pp={p-p_prime}')
-        #         continue
-        #     location = Vector((p_prime[0], origin[1], p_prime[1]))
-        #     o.rotation_euler = (0, 0, 0)
-        #     o.scale = (1, 1, 1)
-        #     o.location = location
-        # print("-" * 60)
-        # Move objects to grid      
-        for o in objects: 
+        o_xyz = np.array([origin[0], origin[1], origin[2]], dtype=np.float32)
+
+        for o in objects:
             print("Location difference before:", o.location - active.location)
-            p = np.array([o.location[0], o.location[2]]) 
-            p_prime = np.around((p - o_xz) / (size_x, size_z)) * (size_x, size_z) + o_xz 
-            if np.allclose(p, p_prime, rtol=1e-21, atol=1e-24): 
-                continue 
-            location = Vector((p_prime[0], origin[1], p_prime[1])) 
-            o.matrix_world @= o.matrix_world.inverted() 
+
+            # Extracting 3D coordinates
+            p = np.array([o.location[0], o.location[1], o.location[2]])
+
+            # Calculations for X, Y, and Z axes
+            p_prime = np.around((p - o_xyz) / (size_x, size_y, size_z)) * (size_x, size_y, size_z) + o_xyz
+
+            # Checking for close match
+            if np.allclose(p, p_prime, rtol=1e-21, atol=1e-24):
+                continue
+
+            # Setting new location
+            location = Vector((p_prime[0], p_prime[1], p_prime[2]))
+            o.matrix_world @= o.matrix_world.inverted()
             o.location = location
-            print("Location difference after:", o.location - active.location)
 
             print("----")
             print('p', p)
             print('p_prime', p_prime)
 
-        # Restore matrix for all and clear parent    
+        # Restore matrix for all and clear parent
         bpy.ops.object.parent_set(type='OBJECT', keep_transform=True)
-        active.matrix_world = mx_orig   
+        active.matrix_world = mx_orig
         bpy.ops.object.parent_clear(type='CLEAR_KEEP_TRANSFORM')
 
         self.report({"INFO"}, "Aligned to grid from active")
