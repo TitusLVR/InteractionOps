23bec7cda0814009883014af25ace566cf94d012
Titus Lavrov <titus.mailbox@gmail.com>
2026-05-28 13:04:36 +0200

    fix(polygon_match): remove unused Iterable import

--- utils/polygon_match.py
+++ utils/polygon_match.py
@@ -11,7 +11,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from collections import Counter
-from typing import Iterable, Sequence
+from typing import Sequence
 
 import numpy as np
 
